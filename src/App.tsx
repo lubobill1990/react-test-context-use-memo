3d9ec8439545067329e349d14ad4a555f7ea8332
Bo Lu <lubobill1990@163.com>
2023-01-09 20:46:06 +0800

    add rerender boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React, { createContext, useMemo, useState } from "react";
+import React, { createContext, memo, useMemo, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+
+const Boundary = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="boundary">
+      <RerenderCount />
+      {children}
+    </div>
+  );
+};
+
+const RerenderCount = () => {
+  const updateCountRef = React.useRef(0);
+
+  return <div>Re-render: {++updateCountRef.current}</div>;
+};
 
 const AppContext = createContext({
   count: 0,
@@ -18,44 +33,45 @@ const AppContextProvider = ({ children }: React.PropsWithChildren) => {
   const [count, setCount] = useState(0);
   const [internalCount, setInternalCount] = useState(0);
   const [text, setText] = useState("");
-  // const value = useMemo(
-  //   () => ({
-  //     count,
-  //     setCount,
-  //     text,
-  //     setText,
-  //     setInternalCount,
-  //   }),
-  //   [count, text, internalCount]
-  // );
+  const value = useMemo(
+    () => ({
+      count,
+      setCount,
+      text,
+      setText,
+      setInternalCount,
+    }),
+    [count, text, internalCount]
+  );
 
-  const value = {
-    count,
-    setCount,
-    text,
-    setText,
-    setInternalCount,
-  };
+  // const value = {
+  //   count,
+  //   setCount,
+  //   text,
+  //   setText,
+  //   setInternalCount,
+  // };
   console.log("AppContextProvider rerender", value);
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
-const Count = () => {
+const Count = memo(() => {
   const { count, setCount } = useAppContext();
   console.log("count rerender", count);
   return (
-    <div className="card">
+    <Boundary>
       <p>Count: {count}</p>
       <button onClick={() => setCount(10)}>Set count to 10</button>
       <button onClick={() => setCount(0)}>Set count to 0</button>
-    </div>
+    </Boundary>
   );
-};
-const InternalCount = () => {
+});
+
+const InternalCount = memo(() => {
   const { setInternalCount } = useAppContext();
   console.log("internal count rerender");
   return (
-    <div className="card">
+    <Boundary>
       <p>Internal Count</p>
       <button onClick={() => setInternalCount(10)}>
         Set internal count to 10
@@ -63,32 +79,34 @@ const InternalCount = () => {
       <button onClick={() => setInternalCount(0)}>
         Set internal count to 0
       </button>
-    </div>
+    </Boundary>
   );
-};
+});
 
-const Paragraph = () => {
+const Paragraph = memo(() => {
   const { setText, text } = useAppContext();
   console.log("paragraph rerender", text);
   return (
-    <div className="card">
+    <Boundary>
       <p>Text: {text}</p>
       <button onClick={() => setText("World")}>Set text to `World`</button>
       <button onClick={() => setText("Hello")}>Set text to `Hello`</button>
-    </div>
+    </Boundary>
   );
-};
+});
 
 function App() {
   return (
     <AppContextProvider>
       <div className="App">
-        <h1>Test React context useMemo</h1>
-        <div className="flex">
-          <InternalCount></InternalCount>
-          <Count></Count>
-          <Paragraph></Paragraph>
-        </div>
+        <Boundary>
+          <h1>Test React context useMemo</h1>
+          <div className="flex">
+            <InternalCount></InternalCount>
+            <Count></Count>
+            <Paragraph></Paragraph>
+          </div>
+        </Boundary>
       </div>
     </AppContextProvider>
   );
